9870f7caf1f6d594153481af8a9179b08aa1e608
Nick <Echophase44@github.com>
2023-07-04 06:45:50 -0400

    Completed Section 7

--- Section 6 - Advanced Types/Section 5 - Classes - Obj/app.js
+++ Section 6 - Advanced Types/Section 5 - Classes - Obj/app.js
@@ -1,1 +1,52 @@
 "use strict";
+// Intersection Type / combining custom types
+const user3 = {
+    name: "Steve",
+    privileges: ["Guard"],
+    startDate: new Date()
+};
+function printEmployeeInformation(emp) {
+    console.log("Name: " + emp.name);
+    if ("privileges" in emp) { //This is the type guard, because we don't know if it'll be Admin or Employee type
+        console.log("Privileges: " + emp.privileges);
+    }
+}
+/////////////////// Another way to type guard ////////////////
+class Car {
+    drive() {
+        console.log("I'm moving!");
+    }
+}
+class Truck {
+    drive() {
+        console.log("This thing is so slow!");
+    }
+    loadCargo(amount) {
+        console.log("Loading cargo: " + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) { // The other Type Guard
+        vehicle.loadCargo(1000);
+    }
+}
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log("Moving at speed: " + speed);
+}
+moveAnimal({ type: 'bird', flyingSpeed: 45 });
+///////////////////////// Type Casting ///////////////////////////
+// const userInputElement = <HTMLInputElement>document.getElementById("user-input"); //Version 1
+const userInputElement = document.getElementById("user-input"); //Version 2 - for use with React, the "!" in front tell TS that the element will never return "null"
+userInputElement.value = "Hello.";
--- Section 6 - Advanced Types/Section 5 - Classes - Obj/notes.md
+++ Section 6 - Advanced Types/Section 5 - Classes - Obj/notes.md
@@ -1,12 +1,7 @@
-## Added things in Typescript
+## Advanced Types
 
-- Public / Private keywords
-- Readonly for properties
-- Constructor shorthand
-- Protected // can be accessed inside the main class and other classes it extends to, "private" ones do not.
-- static keyword infront of methods to make it accessible ouside of the class without creating an instance.
-- Abstract Classes: If you have a method that is abstract, you have to make the class abstract too. Abstract meaning to leave the method empty, to ensure that extended classes have to provide their own conditions in the method.
-
-## Interface
-
-- Describes what an object should look like.
+- Intersection Types
+- Function Overloads
+- Optional Chaining
+- Type Casting
+- Descriminated Union
